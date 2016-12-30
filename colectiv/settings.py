4566876503d30c8f57d3c92cbdc80b0a0afc2489
bogdan-macbook <bogdanned32@gmail.com>
2016-12-30 19:17:56 +0100

    Heroku setup

--- colectiv/settings.py
+++ colectiv/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_collected")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
